d9a8ff311b87913a522487c11ee43dfa38bc85c7
Chanala Wilangka <yaicw49@hotmail.com>
2015-07-23 18:09:55 +0700

    create project

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>PTT</title>
+	<script src="js/jquery-2.1.4.min.js "></script>
+	<script src="js/jquery.fullPage.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1>hello chanala world</h1>
+</body>
+</html>
+
