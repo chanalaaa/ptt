d6e6a5997ded005bff63d2be222604f34efb9323
Chanala Wilangka <yaicw49@hotmail.com>
2015-07-24 12:14:05 +0700

    nav bar

--- views/header.php
+++ views/header.php
@@ -1,11 +1,15 @@
+<meta name="viewport" content="width=device-width">
 <div class="nav_main">
-<div class="nav_area">
-	<ul>
-		<li id="li_nav" ><img id="nav_icon" src="img/header_icon/nav_icon.png " alt="MENU"></li>
-		<li><img id="nav_logo" src="img/header_icon/logo_main.png " alt="PTT"></li>
-		<li><img id="nav_btn" src="img/header_icon/nav_icon.png " alt="MENU"></li>
-	</ul>
-	
-</div>
-	
+    <div class="nav_area">
+        <ul>
+            <li id="li_nav1"><img id="nav_icon" src="img/header_icon/nav_icon.png " alt="MENU"></li>
+        </ul>
+        <div id="nav_logo">
+            <img src="img/header_icon/logo_main.png " alt="PTT">
+        </div>
+        <div id="nav_btn">
+            <a href="#" id="nav_btn1">LOGIN</a>
+            <a href="#" id="nav_btn2">REGISTER</a>
+        </div>
+    </div>
 </div>
