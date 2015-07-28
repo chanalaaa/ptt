242298e113f1ff66e1fff029ab6066de09e4526c
Chanala Wilangka <yaicw49@hotmail.com>
2015-07-28 12:30:35 +0700

    insert pr + fixed position block

--- views/content.php
+++ views/content.php
@@ -9,6 +9,7 @@
         </div>
     </div>
     <div class="wrap">
+        <!-- ****************** TODAY ****************** -->
         <div class="today">
             <article class="today-wrapper">
                 <section class="today-block">
@@ -35,6 +36,7 @@
 </section>
 </article>
 </div>
+<!-- ****************** PRIVILEGE ****************** -->
 <div class="privilege">
     <article class="privilege-wrapper">
         <section class="privilege-block">
@@ -88,6 +90,7 @@
         </section>
     </article>
 </div>
+<!-- ****************** PROMOTION ****************** -->
 <div class="promotion">
     <article class="promotion-wrapper">
         <section class="promotion-block_slide">
@@ -138,8 +141,10 @@
         </section>
     </article>
 </div>
+<!-- ****************** NEWS ****************** -->
 <div class="news">
     <article class="news-wrapper">
+        <!-- ****************** NEWS (LEFT) ****************** -->
         <section class="news-block_left">
             <div class="email">
                 <div class="news_email_title">
@@ -170,27 +175,55 @@
                 </div>
             </div>
         </section>
+        <!-- ****************** NEWS (RIGHT) ****************** -->
         <section class="news-block_right">
             <div class="activity">
-                <div class="ac_header">
-                    <p class="ac_header_title">activity</p>
-                </div>
-                <div class="ac-slide_block">
-                    <ul id="ac-slide_ul">
-                        <li>
-                            <a href="#"><img src="img/promotion/prm_img/1.jpg" alt=""></a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="img/promotion/prm_img/1.jpg" alt=""></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="ac-slide_tab">
-                    <div class="ac-slide_tab_img">
-                        <div class="ac-slide_tab_info">
-                            <p>Pray for Nepal</p>
+                <div class="activity_block">
+                    <div class="ac_header">
+                        <p class="ac_header_title">activity</p>
+                    </div>
+                    <div class="ac-slide_block">
+                        <ul id="ac-slide_ul">
+                            <li>
+                                <a href="#"><img src="img/promotion/prm_img/1.jpg" alt=""></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="img/promotion/prm_img/1.jpg" alt=""></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="ac-slide_tab">
+                        <div class="ac-slide_tab_img">
+                            <div class="ac-slide_tab_info">
+                                <p>Pray for Nepal</p>
+                            </div>
+                            <img src="img/ico/ico_go_dark.png" alt="">
                         </div>
-                        <img src="img/ico/ico_go_dark.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="pr">
+                <div class="pr_block">
+                    <div class="pr_header">
+                        <p class="pr_header_title">activity</p>
+                    </div>
+                    <div class="pr-slide_block">
+                        <ul id="pr-slide_ul">
+                            <li>
+                                <a href="#"><img src="img/promotion/prm_img/1.jpg" alt=""></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="img/promotion/prm_img/1.jpg" alt=""></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="pr-slide_tab">
+                        <div class="pr-slide_tab_img">
+                            <div class="pr-slide_tab_info">
+                                <p>Pray for Nepal</p>
+                            </div>
+                            <img src="img/ico/ico_go_dark.png" alt="">
+                        </div>
                     </div>
                 </div>
             </div>
@@ -202,30 +235,6 @@
 <h1>This is like a normal website.</h1> Just place the rest of your page after the fullpage wrapper and use the option `fitToSection:false` and `autoScrolling:false`. And enjoy a great single slider.
 <script src="https://gist.github.com/alvarotrigo/368e6d6ad3c5c0e98d3c.js"></script>
 <script src="https://gist.github.com/alvarotrigo/c74366b59ecba2f9c3da.js"></script>
-<h1> Rest of your site </h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a sem ultrices neque vehicula fermentum a sit amet nulla. Donec sed tellus tempor ligula condimentum placerat. Vivamus placerat magna nisi, eu elementum risus imperdiet sit amet. Nunc varius dictum porttitor. Morbi rhoncus magna in quam fringilla fringilla in in odio. Quisque fringilla ante vitae tellus fringilla, condimentum tristique mi pharetra. Aenean ultricies odio at erat facilisis tincidunt. Fusce tempor auctor justo, nec facilisis quam vehicula vel. Aenean non mattis purus, eget lobortis odio. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam cursus elit nec aliquam consequat.
-</p>
-<p>
-    Nullam sem orci, tincidunt non lorem quis, ultricies blandit nisl. Donec eget sollicitudin tortor. Integer ut orci sit amet ipsum porta feugiat sit amet ut nulla. Vestibulum auctor, tortor sed scelerisque consectetur, nisl tellus placerat tortor, non euismod nisi risus vitae ipsum. Morbi a velit purus. Nam euismod porta sapien, sed scelerisque nulla lacinia vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam at eleifend ligula, in eleifend sapien.
-</p>
-<div class="box">
-    Curabitur dui elit, tristique eget venenatis et, scelerisque mattis arcu. Pellentesque lectus orci, tempus in enim et, condimentum rutrum magna. Mauris nec eros viverra, facilisis nibh ut, sodales urna. Pellentesque nec neque in ipsum imperdiet egestas vitae vel augue. Integer felis eros, dictum at eleifend aliquet, lacinia non neque. Curabitur eget condimentum urna, eget sodales lectus. Maecenas blandit ac neque id elementum. Phasellus ultricies vestibulum elit ut sagittis. Nam ut porta mi.
-</div>
-<h1> Rest of your site </h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a sem ultrices neque vehicula fermentum a sit amet nulla. Donec sed tellus tempor ligula condimentum placerat. Vivamus placerat magna nisi, eu elementum risus imperdiet sit amet. Nunc varius dictum porttitor. Morbi rhoncus magna in quam fringilla fringilla in in odio. Quisque fringilla ante vitae tellus fringilla, condimentum tristique mi pharetra. Aenean ultricies odio at erat facilisis tincidunt. Fusce tempor auctor justo, nec facilisis quam vehicula vel. Aenean non mattis purus, eget lobortis odio. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam cursus elit nec aliquam consequat.
-</p>
-<p>
-    Nullam sem orci, tincidunt non lorem quis, ultricies blandit nisl. Donec eget sollicitudin tortor. Integer ut orci sit amet ipsum porta feugiat sit amet ut nulla. Vestibulum auctor, tortor sed scelerisque consectetur, nisl tellus placerat tortor, non euismod nisi risus vitae ipsum. Morbi a velit purus. Nam euismod porta sapien, sed scelerisque nulla lacinia vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam at eleifend ligula, in eleifend sapien.
-</p>
-<p>
-    In elementum nec quam et eleifend. Ut nec erat fermentum, mattis leo non, fringilla tellus. Integer at dui nibh. Etiam facilisis fermentum turpis. Nulla malesuada iaculis nisl, ac accumsan felis pulvinar ut. Proin porttitor nulla libero, vel tristique erat faucibus quis. Aliquam pharetra enim et sapien bibendum, interdum viverra lectus sagittis. Vivamus pharetra, risus quis malesuada interdum, mi mauris dignissim mi, ac hendrerit orci nulla vel felis. Vivamus dapibus, nisi vel viverra tincidunt, ligula nunc sagittis elit, eget lacinia tellus velit et leo. Curabitur a tortor pretium, aliquam justo gravida, commodo ipsum. In fermentum lorem eu tincidunt consequat. Donec nec blandit ipsum, id scelerisque velit. Aenean vel ultrices ligula, at imperdiet dolor. Sed euismod turpis et nibh adipiscing feugiat. Etiam diam leo, sollicitudin eu suscipit non, laoreet sit amet dui. In augue purus, semper in blandit ut, suscipit vehicula tortor.
-</p>
-<p>
-    Praesent id varius neque. Nunc risus elit, tincidunt eu nulla vitae, adipiscing porta nibh. Pellentesque dignissim dolor ligula, eu vestibulum justo elementum ac. In a risus ullamcorper, iaculis lectus non, condimentum elit. Pellentesque ac felis nec mauris venenatis elementum. In porttitor mauris sit amet posuere scelerisque. Nunc interdum arcu sit amet libero fermentum, nec consequat risus dignissim.
-</p>
-<p>
-    Curabitur dui elit, tristique eget venenatis et, scelerisque mattis arcu. Pellentesque lectus orci, tempus in enim et, condimentum rutrum magna. Mauris nec eros viverra, facilisis nibh ut, sodales urna. Pellentesque nec neque in ipsum imperdiet egestas vitae vel augue. Integer felis eros, dictum at eleifend aliquet, lacinia non neque. Curabitur eget condimentum urna, eget sodales lectus. Maecenas blandit ac neque id elementum. Phasellus ultricies vestibulum elit ut sagittis. Nam ut porta mi.
-</p>
 </div>
 </div>
 <!-- full page script -->
