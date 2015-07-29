58caba3fd2fdb66a5a74d051ca9354ba02acd1c2
Chanala Wilangka <yaicw49@hotmail.com>
2015-07-29 15:59:54 +0700

    Edit js Hover privilege phase2 [+] pointer-event : none (.privilege-block_body_info_li_p) [+] jquery .stop(true,true) animate

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
         .mouseenter(function() {
             $id = ($(this).index('.privilege-block_body_info_li_tab'));
             animate_tab($id, true);
-            console.log($id);
+            //console.log($id);
         })
         .mouseleave(function() {
             $id = ($(this).index('.privilege-block_body_info_li_tab'));
             animate_tab($id, false);
         });
 
-
+/*
     $(".privilege-block_body_img_li")
         .mouseenter(function() {
             $id = ($(this).index('.privilege-block_body_img_li'));
@@ -22,30 +22,24 @@ $(document).ready(function() {
             animate_tab($id, false);
         });
 
+*/
 
+    
 
-    $(".privilege-block_body_info_li_p")
-        .mouseenter(function() {
-            $id = ($(this).index('.privilege-block_body_info_li_p'));
-            animate_tab($id, true);
-        })
-        .mouseleave(function() {
-            $id = ($(this).index('.privilege-block_body_info_li_p'));
-            animate_tab($id, false);
-        });
-
-/* Hover Func. *------------------------------*/
+    /* Hover Func. *------------------------------*/
 
     function animate_tab($id, $bool) {
 
         if ($bool) {
-            $(".privilege-block_body_info_li_tab").eq($id).animate({
+
+            $(".privilege-block_body_info_li_tab").eq($id).stop(true, true).animate({
                 height: "+=100px",
                 marginTop: "-=100px"
-            }, 200);
+            }, 250);
 
         } else {
-            $(".privilege-block_body_info_li_tab").eq($id).animate({
+
+            $(".privilege-block_body_info_li_tab").eq($id).stop(true, true).animate({
                 height: "-=100px",
                 marginTop: "+=100px"
             }, 500);
